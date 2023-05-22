4bdf61e354a7a4427efde9255996b12631b67c0c
Oliver Ingamells <oliver.ingamells@gmail.com>
2023-05-22 21:35:38 +0100

    Get file path for data folder

--- AdventOfCode2022/AdventOfCode2022.cpp
+++ AdventOfCode2022/AdventOfCode2022.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
-
-#define EXPAND(x) #x
-
+#include <string>
 
 int main()
 {
     
-    const char* s = EXPAND(AOCDATA);
+    //Set up the filepath to the input data
+    std::string sourcePath = __FILE__;
+    std::string dataFolder = "InputData\\Day";
+    std::string extension = "Input.txt";
+    std::string test = "Test";
 
+    //Declare i outside the loop so we can read its value after the loop finishes
+    int i = sourcePath.length() - 1;
 
-    std::cout << "Hello World!\n";
+    for (; i > -1; i--) {
+        if (sourcePath[i] == '\\') {
+            break;
+        }
+    }
+
+    //Resize the string to remove the trailing file name
+    sourcePath.resize(i + 1);
+
+    const char* one = "1";
+
+    std::string loadPath = sourcePath + dataFolder + one + extension;
+
+    std::cout << loadPath;
 
 }
 
